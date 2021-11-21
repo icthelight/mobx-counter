6f62d7a436d7afc30edc8d3cfc6c339de6657aae
hyungji.k <icthelight@naver.com>
2021-11-21 22:48:47 +0900

    mobx counter 예제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import {useObserver} from 'mobx-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import combineStores from './stores/combineStores';
+
+const App = () => {
+  const {counterStore} = combineStores();
+
+  const onClickIncrease = () => {
+    counterStore.increaseAction(1);
+  }
+
+  const onClickDecrease = () => {
+    counterStore.decreaseAction(1);
+  }
+
+  return useObserver(() => (
+    <Container>
+      <Text>현재 값: {counterStore.num}</Text>
+
+      <Button onClick={onClickIncrease}>증가</Button>
+      <Button onClick={onClickDecrease}>감소</Button>
+    </Container>
+  ))
 }
 
+const Container = styled.div`
+  text-align: center;
+  padding-top: 20px;
+`;
+
+const Text = styled.div`
+  margin-bottom: 20px;
+  font-size: 18px;
+`;
+
+const Button = styled.button`
+  margin: 0 10px;
+  padding: 5px;
+  background: #18f;
+  color: #fff;
+  border: none;
+  border-radius: 7px;
+  cursor: pointer;
+  transition: 0.4s;
+  &:hover {
+    background: #d1d1d1;
+  }
+`;
 export default App;
---
+++ src/stores/combineStores.js
@@ -0,0 +1,10 @@
+import counterStore from '../stores/counterStore';
+
+const combineStores = () => ({
+  counterStore,
+  // store1,
+  // store2,
+  // ...
+});
+
+export default combineStores;
